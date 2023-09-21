47dd6ef95ace568c780bf9f9ff08eedc86586432
Альберт <albert1702@ya.ru>
2023-09-21 15:45:30 +0300

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1);
+        System.out.println(bean2);
+
+        ApplicationContext applicationContext1 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat cat1 = (Cat) applicationContext1.getBean("cat");
+        Cat cat2 = (Cat) applicationContext1.getBean("cat");
+        System.out.println(cat1);
+        System.out.println(cat2);
     }
 }
