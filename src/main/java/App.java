03fcaf619ef07f72f4537883defc0fc731a5538c
Альберт <albert1702@ya.ru>
2023-09-22 09:25:26 +0300

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,14 +9,12 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean2 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean1);
-        System.out.println(bean2);
 
         ApplicationContext applicationContext1 =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         Cat cat1 = (Cat) applicationContext1.getBean("cat");
         Cat cat2 = (Cat) applicationContext1.getBean("cat");
-        System.out.println(cat1);
-        System.out.println(cat2);
+        System.out.println(cat1==cat2);
+        System.out.println(bean1==bean2);
     }
 }
